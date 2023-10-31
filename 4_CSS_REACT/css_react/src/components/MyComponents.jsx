6cbb99b741bb1d75939085ece99c35dd3705ed3a
TomoriComAncora <lucas_fernando@ufms.br>
2023-10-31 13:43:16 -0400

    começo expediente tarde

---
+++ 4_CSS_REACT/css_react/src/components/MyComponents.jsx
@@ -0,0 +1,12 @@
+import "./MyComponents.css";
+
+function MyComponents() {
+  return (
+    <div>
+      <h1>Css de componente</h1>
+      <p>Este parágrafo vai ter um estilo</p>
+    </div>
+  );
+}
+
+export default MyComponents;
