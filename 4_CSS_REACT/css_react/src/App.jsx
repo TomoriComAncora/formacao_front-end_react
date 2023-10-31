07e034f8dc3f71a2ab2f552d83fda8d5c962d6e7
TomoriComAncora <lucas_fernando@ufms.br>
2023-10-31 13:35:36 -0400

    FEAT: A DESCRIÇÃO DOQ VC FEZ

---
+++ 4_CSS_REACT/css_react/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+// 2 css de componente
+import MyComponents from "./components/MyComponents";
+
+
+
+function App() {
+  // 4 - css dinâmico inline
+  const n = 14;
+
+  // 5 - classe dinâmica
+  const redTitle = true;
+  return (
+    <div>
+      {/* 1 css global */}
+      <h1>Css no React</h1>
+      {/* 2 css de componente */}
+      <MyComponents/>
+      <p>Teste do css vazado</p>
+      {/* 3 inline style */}
+      <p style={{color: "#4cb4f8", fontSize: "2rem", padding: "23px"}}>Elemento estilizado inline</p>
+      {/* 4 css dinâmico */}
+      <h2 style={n > 10 ? {color: "#8e7cc3", fontSize: "3rem"} : {color: "#93c47d", fontSize: "1rem"}}>CSS dinâmico</h2>
+      {/* 5 classe dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>Este título vai ter uma classe dinâmica</h2>
+    </div>
+  );
+}
+
+export default App;
