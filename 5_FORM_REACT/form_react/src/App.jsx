6d9ed1688fbec754022ddd9e4861a26e00cadde5
TomoriComAncora <lucas_fernando@ufms.br>
2023-10-31 15:45:30 -0400

    fim expediente tarde

---
+++ 5_FORM_REACT/form_react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+// 1 criando form
+import MyForm from './components/MyForm'
+function App() {
+
+
+  return (
+      <div className='App'>
+        <h1>Form em React</h1>
+        {/* 1 criando form */}
+        <MyForm/>
+      </div>
+  )
+}
+
+export default App
