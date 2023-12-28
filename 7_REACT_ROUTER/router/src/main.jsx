ec14b5ccbf514741a6736fd93e4ff75f6ad45016
TomoriComAncora <lucas_fernando@ufms.br>
2023-12-28 09:14:45 -0400

    base

--- 7_REACT_ROUTER/router/src/main.jsx
+++ 7_REACT_ROUTER/router/src/main.jsx
@@ -2,9 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Contact from './routes/Contact.jsx'
+
+// 1 configurando router
+import {createBrowserRouter, RouterProvider, Route} from "react-router-dom";
+
+// 2 pagina de erro
+import ErrorPage from './routes/ErrorPage.jsx'
+
+// 3 componente base
+import Home from './routes/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      }
+    ]
+  },
+  // {
+  //   path: "contact",
+  //   element: <Contact/>
+  // }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ 7_REACT_ROUTER/router/src/routes/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import { useRouteError } from "react-router-dom"
+
+function ErrorPage() {
+    const error = useRouteError();
+
+    console.error(error);
+  return (
+    <div>
+        <h1>Ops!</h1>
+        <p>Você teve um erro!</p>
+        <p>{error.statusText}</p>
+        <p>{error.error.message}</p>
+        <p>{error.status}</p>
+    </div>
+  )
+}
+
+export default ErrorPage
